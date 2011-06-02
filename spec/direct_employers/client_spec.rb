5cf4dd04bf473b2f73631d1f9882d2f32f7553cb
Ryan Resella <ryan@codeforamerica.org>
2011-06-02 15:51:51 -0700

    Added specs for querying 11b

--- spec/direct_employers/client_spec.rb
+++ spec/direct_employers/client_spec.rb
@@ -18,5 +18,18 @@ describe DirectEmployers::Client do
         search.api.error.should == "Search Too Broad"
       end
     end
+    
+    context "with query parameter moc code 11b" do
+      before do
+        stub_get("api.asp?key=abc123&moc=11b").
+          to_return(:status => 200, :body => fixture("11bquery.xml"))
+      end
+        
+      it "should return a list of jobs with moc code 11b" do
+        search = @client.search(:key => "abc123", :moc => "11b")
+        a_get("api.asp?key=abc123&moc=11b").should have_been_made
+        search.api.query.should == "First-Line Supervisors/Managers of Weapons Specialists/Crew Members, Infantry"
+      end
+    end
   end
 end
