95054aa43821ac26d4c0a00f76b74bbd89326219
Ryan Resella <ryan@codeforamerica.org>
2011-06-01 17:13:45 -0700

    Initial file for client specs

--- lib/direct_employers.rb
+++ lib/direct_employers.rb
@@ -1,3 +1,15 @@
 module DirectEmployers
-  # Your code goes here...
+  def self.new
+    DirectEmployers::Client.new
+  end
+  
+  # Delegate to DirectEmployers::Client
+  def self.method_missing(method, *args, &block)
+    return super unless new.respond_to?(method)
+    new.send(method, *args, &block)
+  end
+
+  def self.respond_to?(method, include_private=false)
+    new.respond_to?(method, include_private) || super(method, include_private)
+  end
 end
---
+++ spec/direct_employers/client_spec.rb
@@ -0,0 +1,7 @@
+require 'helper'
+
+describe DirectEmployers::Client do
+  before do
+    @client = DirectEmployers::Client.new
+  end
+end
