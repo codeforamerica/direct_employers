e85fc770c3072ffd976ee97ebb71a20357869b6e
Ryan Resella <ryan@codeforamerica.org>
2011-06-01 17:18:42 -0700

    Added connection file

---
+++ lib/direct_employers/connection.rb
@@ -0,0 +1,17 @@
+require 'faraday_middleware'
+
+module DirectEmployers
+  module Connection
+    private
+
+    def connection
+      Faraday.new(:url => 'http://www.jobcentral.com/api.asp') do |connection|
+        connection.use Faraday::Request::UrlEncoded
+        connection.use Faraday::Response::RaiseError
+        connection.use Faraday::Response::Rashify
+        connection.use Faraday::Response::ParseJson
+        connection.adapter(Faraday.default_adapter)
+      end
+    end
+  end
+end
