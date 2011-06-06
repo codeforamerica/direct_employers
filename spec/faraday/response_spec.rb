5b58e2470bf43c15b57c1f093acb36e894335ec1
Ryan Resella <ryan@codeforamerica.org>
2011-06-06 14:10:55 -0700

    Added spec for faraday response

---
+++ spec/faraday/response_spec.rb
@@ -0,0 +1,33 @@
+require 'helper'
+
+describe Faraday::Response do
+  before do
+    @client = DirectEmployers::Client.new
+  end
+
+  {
+    400 => Faraday::Error::ClientError,
+    401 => Faraday::Error::ClientError,
+    403 => Faraday::Error::ClientError,
+    404 => Faraday::Error::ResourceNotFound,
+    406 => Faraday::Error::ClientError,
+    500 => Faraday::Error::ClientError,
+    501 => Faraday::Error::ClientError,
+    502 => Faraday::Error::ClientError,
+    503 => Faraday::Error::ClientError,
+  }.each do |status, exception|
+    context "when HTTP status is #{status}" do
+
+      before do
+        stub_get("api.asp?key=abc123&moc=11b").
+          to_return(:status => status, :body => fixture("11bquery.xml"))
+      end
+
+      it "should raise #{exception.name} error" do
+        lambda do
+          @client.search(:key=> "abc123", :moc => "11b")
+        end.should raise_error exception
+      end
+    end
+  end
+end
