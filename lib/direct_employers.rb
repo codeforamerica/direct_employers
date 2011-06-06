71ea8cd6c38b35750800297ee3aa209c74e1058d
Ryan Resella <ryan@codeforamerica.org>
2011-06-06 13:32:52 -0700

    Changed New to accept options which should be the API key

--- lib/direct_employers.rb
+++ lib/direct_employers.rb
@@ -1,17 +1,18 @@
 require 'direct_employers/client'
 
 module DirectEmployers
-  def self.new
-    DirectEmployers::Client.new
+  
+  def self.client(options={})
+    DirectEmployers::Client.new(options)
   end
 
   # Delegate to DirectEmployers::Client
   def self.method_missing(method, *args, &block)
-    return super unless new.respond_to?(method)
-    new.send(method, *args, &block)
+    return super unless client.respond_to?(method)
+    client.send(method, *args, &block)
   end
 
   def self.respond_to?(method, include_private=false)
-    new.respond_to?(method, include_private) || super(method, include_private)
+    client.respond_to?(method, include_private) || super(method, include_private)
   end
 end
