c8e6015174838b493ea3d28bc380b3c85ec68208
Ryan Resella <ryan@codeforamerica.org>
2011-06-06 15:39:03 -0700

    Fix specs

--- spec/direct_employers_spec.rb
+++ spec/direct_employers_spec.rb
@@ -5,13 +5,13 @@ describe DirectEmployers do
     
   describe ".respond_to?" do
     it "should be true if method exists" do
-      DirectEmployers.respond_to?(:client, true).should be_true
+      DirectEmployers.respond_to?(:new, true).should be_true
     end
   end
 
-  describe ".client" do
+  describe ".new" do
     it "should be a DirectEmployers::Client" do
-      DirectEmployers.client.should be_a DirectEmployers::Client
+      DirectEmployers.new.should be_a DirectEmployers::Client
     end
   end
   
