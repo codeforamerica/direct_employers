cdef61648c54745d81fc9669feea1f17d17866e9
Ryan Resella <ryan@codeforamerica.org>
2011-06-06 13:25:01 -0700

    Added specs for testing method missing

--- spec/direct_employers_spec.rb
+++ spec/direct_employers_spec.rb
@@ -1,9 +1,18 @@
+# -*- encoding: utf-8 -*-
 require 'helper'
 
 describe DirectEmployers do
-  describe ".new" do
-    it "should return a DirectEmployers::Client" do
-      DirectEmployers.new.should be_a DirectEmployers::Client
+    
+  describe ".respond_to?" do
+    it "should be true if method exists" do
+      DirectEmployers.respond_to?(:client, true).should be_true
     end
   end
+
+  describe ".client" do
+    it "should be a DirectEmployers::Client" do
+      DirectEmployers.client.should be_a DirectEmployers::Client
+    end
+  end
+  
 end
