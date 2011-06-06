30b1b324a24a4e3f7f69407a57d9de619d9ff6cc
Ryan Resella <ryan@codeforamerica.org>
2011-06-06 15:38:11 -0700

    Revert "Changed New to accept options which should be the API key"
    This reverts commit 71ea8cd6c38b35750800297ee3aa209c74e1058d.

--- lib/direct_employers.rb
+++ lib/direct_employers.rb
@@ -1,18 +1,17 @@
 require 'direct_employers/client'
 
 module DirectEmployers
-  
-  def self.client(options={})
-    DirectEmployers::Client.new(options)
+  def self.new
+    DirectEmployers::Client.new
   end
 
   # Delegate to DirectEmployers::Client
   def self.method_missing(method, *args, &block)
-    return super unless client.respond_to?(method)
-    client.send(method, *args, &block)
+    return super unless new.respond_to?(method)
+    new.send(method, *args, &block)
   end
 
   def self.respond_to?(method, include_private=false)
-    client.respond_to?(method, include_private) || super(method, include_private)
+    new.respond_to?(method, include_private) || super(method, include_private)
   end
 end
