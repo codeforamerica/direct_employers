23d566e5a42177fe0aa645343b836821f01336db
Erik Michaels-Ober <sferik@gmail.com>
2011-06-23 19:12:10 -0700

    Wrap all module methods in class << self

--- lib/direct_employers.rb
+++ lib/direct_employers.rb
@@ -1,17 +1,19 @@
 require 'direct_employers/client'
 
 module DirectEmployers
-  def self.new(options={})
-    DirectEmployers::Client.new(options)
-  end
+  class << self
+    def new(options={})
+      DirectEmployers::Client.new(options)
+    end
 
-  # Delegate to DirectEmployers::Client
-  def self.method_missing(method, *args, &block)
-    return super unless new.respond_to?(method)
-    new.send(method, *args, &block)
-  end
+    # Delegate to DirectEmployers::Client
+    def method_missing(method, *args, &block)
+      return super unless new.respond_to?(method)
+      new.send(method, *args, &block)
+    end
 
-  def self.respond_to?(method, include_private=false)
-    new.respond_to?(method, include_private) || super(method, include_private)
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
   end
 end
